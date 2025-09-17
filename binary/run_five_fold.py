011f5cb7786bd83add5246196466261178c352fa
xiaofeng-codes <9527superman@gmail.com>
2025-09-17 17:14:41 +0800

    Update run_five_fold.py

--- binary/run_five_fold.py
+++ binary/run_five_fold.py
@@ -4,11 +4,12 @@ import sys
 def main():
     for dataset in['zhang']:
         print(f"Calling train.py with argument: {dataset}")
-        for i in [0]:
+        for i in [0, 1, 2, 3, 4]:
             fold = str(i)
             print(f"Calling train.py with argument: {fold}")
             subprocess.run([sys.executable, "train.py", "--fold", fold, "--dataset", dataset])
 
 
 if __name__ == "__main__":
+
     main()
